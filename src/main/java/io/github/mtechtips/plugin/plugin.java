b7bf1d3b123ccf40c5be12d692eaaedc4a3ed870
MTechTips <milessmilesevans@icloud.com>
2021-05-17 19:21:26 +0100

    Initial commit

---
+++ src/main/java/io/github/mtechtips/plugin/plugin.java
@@ -0,0 +1,14 @@
+package io.github.mtechtips.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class plugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+}
