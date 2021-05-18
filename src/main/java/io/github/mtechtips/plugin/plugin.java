5d23fd37bade1ef2a0b5db2e7fdc94192b06128f
MTechTips <milessmilesevans@icloud.com>
2021-05-18 12:25:12 +0100

    Updated to be in correct format

--- src/main/java/io/github/mtechtips/plugin/plugin.java
+++ src/main/java/io/github/mtechtips/plugin/plugin.java
@@ -5,10 +5,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class plugin extends JavaPlugin {
     @Override
     public void onEnable() {
+
         getLogger().info("onEnable is called!");
     }
     @Override
     public void onDisable() {
+
         getLogger().info("onDisable is called!");
     }
 }
